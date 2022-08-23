874cec5c1226ab362acc48a757f957fe0d377112
CHIBX <111882496+CHIBX@users.noreply.github.com>
2022-08-23 14:50:56 +0100

    Add files via upload

---
+++ chibx/chibx.js
@@ -0,0 +1,27 @@
+console.log("Loading...");
+var points = 42000
+var x = 140
+var y = 14
+console.log(x*y);
+let coins =19610;
+console.log(coins);
+if(points<500){
+console.log("Game Over");}
+else{console.log("Continue")}
+if(coins<=10000){
+console.log("You Lose");	
+}
+else{
+console.log("Keep Playing");	
+}
+function jump(right)
+console.log("jump to"+right)
+function jump(left)
+console.log("jump to"+left)
+jump(up)
+jump(left)
+jump(right)
+{console.log("points");}
+{points= 400};
+alert("Game Over")
+Game{console.log("Begin");}
